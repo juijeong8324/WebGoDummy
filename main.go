d96a0587171c970049415a31752b7d0a0d71ae53
juijeong8324 <juijeong8324@gmail.com>
2023-09-07 21:11:25 +0900

    update main.go 1.0

--- main.go
+++ main.go
@@ -7,11 +7,7 @@ import (
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
-<<<<<<< Updated upstream
-	fmt.Fprintf(w, "Hello, I'm uiJeong! -v6.0")
-=======
 	fmt.Fprintf(w, "Hello, UiJeong!-v6.0")
->>>>>>> Stashed changes
 }
 
 func main() {
